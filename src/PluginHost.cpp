0df81bae6b405618c9d04a89ebeb55e9de00bb9f
Robin Davies <rerdavies@gmail.com>
2023-07-03 09:22:20 -0400

    Wrong text for lables.

--- src/PluginHost.cpp
+++ src/PluginHost.cpp
@@ -663,10 +663,8 @@ Lv2PluginInfo::Lv2PluginInfo(PluginHost *lv2Host, LilvWorld *pWorld, const LilvP
     this->label_ = nodeAsString(label);
     if (label_.length() == 0)
     {
-        this->label_ = name;
-    } else {
-        std::cout << this->label_ << std::endl;
-    }
+        this->label_ = this->name_;
+    } 
 
 
     AutoLilvNode author_name = (lilv_plugin_get_author_name(pPlugin));
