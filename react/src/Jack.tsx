51193e8312cb2b989ca6b6275c912832ff40795f
Robin Davies <rerdavies@gmail.com>
2024-10-12 05:55:27 -0400

    Build machine break.

--- react/src/Jack.tsx
+++ react/src/Jack.tsx
@@ -17,7 +17,6 @@
 // IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 // CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-import {PiPedalArgumentError} from './PiPedalError';
 import {AlsaMidiDeviceInfo} from './AlsaMidiDeviceInfo';
 
 function getChannelNumber(channelId: string): number {
